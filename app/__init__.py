ff0fe9ea5a30e71bc5774bd9c5f43e7eab179f2b
xiaotongtong <49009613+thorraythorray@users.noreply.github.com>
2023-01-13 18:13:27 +0800

    add .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 
+from etc.config import ROOT, ENV
 from app.maicai import create_bp as maicai_bp
 
 
 def register_blueprints(app):
     app.register_blueprint(maicai_bp(), url_prefix="/maicai")
+
+
+def load_app_config(app):
+    load_dotenv(os.path.join(ROOT, "app/config/{env}.env").format(env=ENV))
+    app.config.from_object("app.config.{env}.{Env}Config".format(env=env, Env=env.capitalize()))
 
 
 def create_app():
--- etc/config.py
+++ etc/config.py
@@ -2,3 +2,4 @@ import os
 
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENV = 'dev'
