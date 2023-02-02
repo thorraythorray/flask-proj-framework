f33b6b0440c86d45099fbda9d81252f617aabb0d
xiaotongtong <49009613+thorraythorray@users.noreply.github.com>
2023-02-02 15:42:25 +0800

    add guicorn conf

---
+++ gunicorn.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import multiprocessing
+import pathlib
+import os
+
+LOG_PATH = '/var/log/gunicorn'
+if not os.path.exists(LOG_PATH):
+    os.makedirs(LOG_PATH)
+
+workers = max(2, min(int(multiprocessing.cpu_count() / 2) + 1, 5))
+bind = '127.0.0.1:9001'
+worker_class = 'gevent'
+
+threads = 2
+worker_connections = 1000
+max_requests = 2000
+keepalive = 2
+timeout = 300
+graceful_timeout = 30
+
+reload = True
+debug = False
+daemon = False  # 启动后台进程（当supervisor管理进程时需要False)
+
+pidfile = '/run/pyfla.pid'
+chdir = str(pathlib.Path().resolve().parent.parent)
+loglevel = 'info'
+errorlog = '/var/log/gunicorn/error.log'
+accesslog = '/var/log/gunicorn/access.log'
