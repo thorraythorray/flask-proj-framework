66b3328a967f8f362e4765adc851c1f947ea0a7f
xiaotongtong <49009613+thorraythorray@users.noreply.github.com>
2023-02-03 14:21:47 +0800

    test nest bluprint

--- app/maicai/__init__.py
+++ app/maicai/__init__.py
@@ -8,8 +8,14 @@ class BaseModel(db.Model, BaseModelMixin):
     __abstract__ = True
 
 
-def create_maicai_bp():
-    maicai_bp = Blueprint("maicai", __name__, url_prefix='/maicai')
-    from app.maicai.views import bp as api_bp
-    maicai_bp.register_blueprint(api_bp, url_prefix="/api")
-    return maicai_bp
+# def create_maicai_bp():
+#     maicai_bp = Blueprint("maicai", __name__, url_prefix='/maicai')
+#     from app.maicai.views import bp as api_bp
+#     maicai_bp.register_blueprint(api_bp)
+#     return maicai_bp
+
+
+bp = Blueprint("maicai", __name__, url_prefix='/maicai')
+
+# __import__('app.maicai.views', fromlist=['bp'])
+from app.maicai.views import *
--- app/maicai/views.py
+++ app/maicai/views.py
@@ -1,9 +1,10 @@
 from flask import jsonify
-from flask import Blueprint
+# from flask import Blueprint
 
 from utils.log import logger
+from app.maicai import bp
 
-bp = Blueprint("maicai", __name__, url_prefix="/api")
+# bp = Blueprint("api", __name__)
 
 @bp.route("/test", methods=["GET"])
 def test():
