451e0bcfe518bc139dcd3b88289d1a9bb502d68a
xiaotongtong <49009613+thorraythorray@users.noreply.github.com>
2023-02-07 14:03:14 +0800

    lint fix

--- tasks.py
+++ tasks.py
@@ -3,6 +3,6 @@ from invoke import task
 
 @task
 def lint(c):  # pylint: disable=invalid-name
-    c.run('pylint --rcfile=.pylintrc app utils commands')
+    c.run('pylint --rcfile=.pylintrc app utils')
 
 # Collection(lint)  # pylint: disable=invalid-name
